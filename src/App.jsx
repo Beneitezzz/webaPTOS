f0b091c4b3844f6c2a9d0ae4d3bce482ea7d6038
ba-rolon <mati_beneitez@hotmail.com>
2026-05-29 08:55:12 -0300

    feat: wire AuthProvider, PrivateRoute and AdminRoute into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AppProvider } from './context/AppContext'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
+import PrivateRoute from './components/PrivateRoute'
+import AdminRoute from './components/AdminRoute'
 import Home from './pages/Home'
 import Mapa from './pages/Mapa'
 import Perfil from './pages/Perfil'
 import DetalleComercio from './pages/DetalleComercio'
 import RegistroComercio from './pages/RegistroComercio'
 import AdminPanel from './pages/AdminPanel'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 export default function App() {
   return (
-    <AppProvider>
-      <BrowserRouter>
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mapa" element={<Mapa />} />
-            <Route path="/perfil" element={<Perfil />} />
-            <Route path="/comercio/:id" element={<DetalleComercio />} />
-            <Route path="/registro-comercio" element={<RegistroComercio />} />
-            <Route path="/admin" element={<AdminPanel />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <BrowserRouter>
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/mapa" element={<Mapa />} />
+              <Route path="/comercio/:id" element={<DetalleComercio />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/perfil" element={<PrivateRoute><Perfil /></PrivateRoute>} />
+              <Route path="/registro-comercio" element={<PrivateRoute><RegistroComercio /></PrivateRoute>} />
+              <Route path="/admin" element={<AdminRoute><AdminPanel /></AdminRoute>} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </AppProvider>
+    </AuthProvider>
   )
 }
